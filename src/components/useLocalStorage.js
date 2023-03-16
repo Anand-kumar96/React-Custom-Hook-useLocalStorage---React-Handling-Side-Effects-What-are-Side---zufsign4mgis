fd8193f53733d44d5a8b1fb9517b6b06c1ff2a3f
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-17 01:40:06 +0530

    Update useLocalStorage.js

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,33 +2,19 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-
-    const [storedValue, setStoredValue] = useState(() => {
-        if (typeof window === "undefined") {
-          return value;
-        }
-        try {
+     const [storedValue, setStoredValue] = useState(() => {
           const item = window.localStorage.getItem(stateVar);
           return item ? JSON.parse(item) : value;
-        } catch (error) {
-          console.log(error);
-          return value;
+        });
+      
+        // create a function that updates the value in local storage and updates the state
+        const setValue = (value) => {
+          setStoredValue(value);
+          window.localStorage.setItem(stateVar, value);
         }
-      });
-
-      const setValue = (value) => {
-        try {
-          const valueToStore = value instanceof Function ? value(storedValue) : value;
-          setStoredValue(valueToStore);
-          if (typeof window !== "undefined") {
-            window.localStorage.setItem(stateVar, JSON.stringify(valueToStore));
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      };
-
-      return [storedValue, setValue];
+      
+        // return the current value and the function to update it
+        return [storedValue, setValue];
 };
 
 export default useLocalStorage;
